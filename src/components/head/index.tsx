7e9c06ede81098244692a651ececb6cf4e7c0aae
pjflanagan <pjflanagan1@gmail.com>
2021-10-06 00:09:33 -0400

    apple-mobile-web-app-status-bar-style

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -24,6 +24,8 @@ export const HeadComponent = () => {
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
       <title>{TITLE}</title>
 
       {/* Social */}
